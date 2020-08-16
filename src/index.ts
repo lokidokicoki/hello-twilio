7f3ea9782b090cc875a7bcf3ea15365745caa026
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-16 10:49:18 +0100

    pull creds from env file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
+
+require('dotenv').config({path:'./twilio.env'});
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
