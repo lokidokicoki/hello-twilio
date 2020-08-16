7a1846c8bd54de33d7d47bdf2c804d76ef0bd060
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-16 19:54:30 +0100

    feat(deps): include material-ui, clean up app form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,29 @@
 import React, {useState} from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme)=>({
+  paper:{
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection:'column',
+    alignItems: 'center'
+  },
+  form:{
+    width: '100%',
+    marginTop: theme.spacing(1)
+  },
+  submit:{
+    margin: theme.spacing(3,0,2)
+  }
+}));
 
 function App() {
+  const classes = useStyles();
   const [telno, setTelno] = useState('');
   const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
     setTelno(e.target.value);
@@ -10,19 +33,19 @@ function App() {
     alert(`number: ${telno}`)
   }
   return (
-    <div className="App">
-      <header className="App-header">
+    <Container component="main" maxWidth="xs">
+      <CssBaseline/>
+      <div className={classes.paper}>
+
+      <Typography component="h1" variant="h5">
         Hello Twilio
-      </header>
-      <div>
-        <form noValidate autoComplete="off" onSubmit={handleSubmit}>
-          <label>Enter phone number, hit 'Submit' and get a friendly message!
-          <input name="telno" value={telno} onChange={handleChange} type="text" placeholder="Enter phone number here"></input>
-          </label>
-        <input type="submit" value="Submit"/>
+      </Typography>
+        <form className={classes.form} noValidate autoComplete="off" onSubmit={handleSubmit}>
+          <TextField variant="outlined" fullWidth margin="normal" required name="telno" label="Enter phone number" value={telno} onChange={handleChange} type="text" placeholder="e.g. +447740984037"/>
+        <Button variant="contained" fullWidth color="primary" className={classes.submit} type="submit" value="Submit">Submit</Button>
         </form>
       </div>
-    </div>
+    </Container>
   );
 }
 
