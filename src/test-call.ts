c572d6c2616963d048a32043235b664cd3cd6ff6
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-16 14:40:36 +0100

    test-call: rewire to override twiml, more traditional greeting

--- src/test-call.ts
+++ src/test-call.ts
@@ -10,7 +10,7 @@ const client = require('twilio')(accountSid, authToken);
 
 client.calls
       .create({
-         url: 'http://demo.twilio.com/docs/voice.xml',
+         twiml: '<Response><Say>Hello world</Say></Response>',
          to: '+447740984037',
          from: '+12056497881'
        })
