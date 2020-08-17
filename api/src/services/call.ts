467e315b03f21f480864729570c1034229f77c8a
Robin Tregaskis <lokidokicoki@gmail.com>
2020-08-17 08:09:43 +0100

    feat(api): revive live code

--- api/src/services/call.ts
+++ api/src/services/call.ts
@@ -6,16 +6,14 @@ const client = require('twilio')(accountSid, authToken);
 const fromNumber = '+12056497881';
 
 export async function callNumber(telno: string): Promise<string> {
-    // const call = await client.calls.create({
-    //     twiml: '<Response><Say>Hello</Say></Response>',
-    //     to: telno,//'+447740984037',
-    //     from: fromNumber
-    // });
+    const call = await client.calls.create({
+        twiml: '<Response><Say>Hello</Say></Response>',
+        to: telno,//'+447740984037',
+        from: fromNumber
+    });
 
-    const call = {
-        sid: Math.random().toString()
-    }
-//throw new Error(`pistonbroke`)
-    //      .then((call:any) => console.log(call.sid));
+    // const call = {
+    //     sid: Math.random().toString()
+    // }
     return call.sid;
 }
